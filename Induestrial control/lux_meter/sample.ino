0a5e24d34d0185fd5c0f5c95f09ea18efbec48c5
Md Hasemi Rafsan Jani Shohan <113755788+Rafsan12345@users.noreply.github.com>
2022-12-07 11:25:58 +0600

    Add files via upload

---
+++ Induestrial control/lux_meter/sample.ino
@@ -0,0 +1,47 @@
+#include <LiquidCrystal_I2C.h>
+#include "DHT.h"
+#define DHTPIN 2
+#define DHTTYPE DHT11
+DHT dht(DHTPIN, DHTTYPE);
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+int ldrPin = A0;
+float ldrStatus = 0;
+
+void setup()
+{
+  lcd.init();
+  lcd.clear();
+  lcd.backlight();      // Make sure backlight is on
+  Serial.begin(9600);
+  Serial.println(F("DHTxx test!"));
+  dht.begin();
+  pinMode(ldrPin, INPUT);
+}
+
+void loop()
+{
+  lcd.clear();
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+  float r2 = 220;
+  float ldrStatus = analogRead(ldrPin);
+  float v2 = (ldrStatus / 204.6);
+  float v1 = (5 - v2);
+  float r1 = (v1 * 220) / (5 - v1);
+  int lux =  (500 / (r1 / 1000));
+
+  //Serial.println(ldrStatus);
+  lcd.setCursor(1, 0);  //Set cursor to character 2 on line 0
+  lcd.print(t);
+  lcd.print('C');
+
+  lcd.setCursor(8, 0);  //Set cursor to character 2 on line 0
+  lcd.print(h);
+  lcd.print('%');
+  lcd.setCursor(1, 1);  //Set cursor to character 2 on line 0
+  lcd.print("LUX>> ");
+  lcd.print(lux);
+  
+  delay(200);
+  
+}
